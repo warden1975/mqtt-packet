c52eb173b00c96f300c9616e2242cff8c239e4fe
Warren Gutierrez <warren.gutierrez@gmail.com>
2018-03-18 16:08:34 +0800

    Move nextTick function directly
    For some reason I can't have the process-nextick-args being read from node.

--- writeToStream.js
+++ writeToStream.js
@@ -5,13 +5,46 @@ var Buffer = require('safe-buffer').Buffer
 var empty = Buffer.allocUnsafe(0)
 var zeroBuf = Buffer.from([0])
 var numbers = require('./numbers')
-var nextTick = require('process-nextick-args').nextTick
 
 var numCache = numbers.cache
 var generateNumber = numbers.generateNumber
 var generateCache = numbers.generateCache
 var writeNumber = writeNumberCached
 var toGenerate = true
+
+function nextTick(fn, arg1, arg2, arg3) {
+  if (typeof fn !== 'function') {
+    throw new TypeError('"callback" argument must be a function');
+  }
+  var len = arguments.length;
+  var args, i;
+  switch (len) {
+  case 0:
+  case 1:
+    return process.nextTick(fn);
+  case 2:
+    return process.nextTick(function afterTickOne() {
+      fn.call(null, arg1);
+    });
+  case 3:
+    return process.nextTick(function afterTickTwo() {
+      fn.call(null, arg1, arg2);
+    });
+  case 4:
+    return process.nextTick(function afterTickThree() {
+      fn.call(null, arg1, arg2, arg3);
+    });
+  default:
+    args = new Array(len - 1);
+    i = 0;
+    while (i < args.length) {
+      args[i++] = arguments[i];
+    }
+    return process.nextTick(function afterTick() {
+      fn.apply(null, args);
+    });
+  }
+}
 
 function generate (packet, stream) {
   if (stream.cork) {
